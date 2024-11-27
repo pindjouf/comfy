064a6cb7c22049ffd600e80f48c98b0feb601243
magix <esaubukasa@protonmail.com>
2024-11-27 06:11:15 +0100

    fix vercel!!!

--- landing_page/svelte.config.js
+++ landing_page/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter({
-            runtime: 'nodejs18.x',
+            runtime: 'nodejs20.x',
         }),
 	}
 };
